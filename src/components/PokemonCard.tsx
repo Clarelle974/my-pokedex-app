73bfd14c25e1d82e62779eca538cf75c9e7c178e
Clarelle974 <clarelle.lemonnier@gmail.com>
2024-12-11 12:48:36 +0100

    l'img s'affiche s'il existe une src, sinon un <p> s'affiche

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,11 +1,23 @@
 function PokemonCard() {
-    return <div>
-                <figure>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
-                    alt="Bulbasaur" />
-                </figure>
-                <figcaption>Bulbasaur</figcaption>
-    </div>;
-  }
-  
-  export default PokemonCard;
+  return (
+    <div>
+      <figure>{pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+      </figure>
+      <figcaption>{pokemon.name}</figcaption>
+    </div>
+  );
+}
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+const pokemon = pokemonList[0];
+
+export default PokemonCard;
