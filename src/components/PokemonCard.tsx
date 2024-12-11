852d16ec3667d93a00181ce3f548b99f4912d188
Clarelle974 <clarelle.lemonnier@gmail.com>
2024-12-11 16:30:22 +0100

    quête 04 - ajout de props

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,4 +1,5 @@
-function PokemonCard() {
+function PokemonCard({pokemon}) {
+    console.log(pokemon);
   return (
     <div>
       <figure>{pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
@@ -8,16 +9,5 @@ function PokemonCard() {
   );
 }
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-const pokemon = pokemonList[0];
 
 export default PokemonCard;
